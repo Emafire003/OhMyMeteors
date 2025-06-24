6553d959286cdfc3cf8efdf4accc41bd2353e22b
Slayer5934 <slayer5934@protonmail.com>
2025-06-23 22:36:08 -0400

    Make it render ._.

--- src/main/java/me/emafire003/dev/ohmymeteors/blocks/basic_laser/BasicMeteorLaserBlock.java
+++ src/main/java/me/emafire003/dev/ohmymeteors/blocks/basic_laser/BasicMeteorLaserBlock.java
@@ -65,6 +65,12 @@ public class BasicMeteorLaserBlock extends BlockWithEntity implements BlockEntit
         return new BasicMeteorLaserBlockEntity(pos, state);
     }
 
+    //The opacity doesn't seem to work ingame :( apparently partially transparent blocks are not supported by default..
+    @Override
+    protected BlockRenderType getRenderType(BlockState state) {
+        return BlockRenderType.MODEL;
+    }
+
     @Nullable
     @Override
     public <T extends BlockEntity> BlockEntityTicker<T> getTicker(World world, BlockState state, BlockEntityType<T> type) {
